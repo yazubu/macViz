f4ea0ab5b3bafc10944582911061ee5577af2aae
Valdeq <waldes@bitflow.eu>
2026-06-12 10:33:43 +0200

    ableton fix

--- obj/Debug/net10.0/macViz.AssemblyInfo.cs
+++ obj/Debug/net10.0/macViz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("macViz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e791864f1a5cafb677bacf4db3f1f61030106fa9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71e3af4dadf4af08c66798ee4305725136248275")]
 [assembly: System.Reflection.AssemblyProductAttribute("macViz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("macViz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
